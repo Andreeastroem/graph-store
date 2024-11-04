cdfabd7c25f32320a200360d50783f628ec3e582
kjof <andreeastroem@gmail.com>
2024-11-04 20:51:04 +0100

    fix: build issues regarding linting and default image

--- components/ui/image.tsx
+++ components/ui/image.tsx
@@ -3,5 +3,5 @@
 import { CldImage, CldImageProps } from "next-cloudinary";
 
 export default function Image(props: CldImageProps) {
-  return <CldImage {...props} defaultImage="cld-sample-5" />;
+  return <CldImage {...props} defaultImage="book-not-found.webp" />;
 }
